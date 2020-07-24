a71aa8f326a044bf4e16181161a9fee5000c7ece
tawatamasato <donafudo@gmail.com>
2020-07-24 09:42:36 +0900

    add: accept command line arguments

--- src/ModelTest.py
+++ src/ModelTest.py
@@ -1,10 +1,11 @@
 from PIL import Image
 import PIL.ImageOps
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 import keras
 import cv2
+import sys
+import os
 
 model = keras.models.load_model("./model_001")
 
@@ -14,11 +15,15 @@ size = 40
 s=pd.Series(list("あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽっゃゅょ"))
 labels=pd.get_dummies(s).columns
 
-for num in range(1):
+for img_path in sys.argv[1:]:
 
-    test_image = "./testImgs/{}.jpg".format("hu")
+    if(os.path.isfile(img_path)==False):
+        try:
+            raise FileNotFoundError('no such file')
+        except FileNotFoundError:
+            raise
     
-    image = cv2.imread(test_image, 0)
+    image = cv2.imread(img_path, 0)
     
     ret, image = cv2.threshold(image, 230, 255, cv2.THRESH_BINARY)
     #resize image
@@ -34,12 +39,9 @@ for num in range(1):
     in_fact_data = in_fact_data / 255 
     in_fact_data = np.reshape(in_fact_data,(1,size,size,1))
 
-    plt.show()
-
     expect = model.predict(in_fact_data)
-    print("value:{}".format("hu"))
     args=expect[0].argsort()[::-1]
     for i in args[:3]:
         print("predict:{}".format(labels[i]))
-
         print(expect[0][i])
+        print()
